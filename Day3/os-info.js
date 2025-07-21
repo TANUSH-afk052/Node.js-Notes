6c181185dbcb3a9ce1e6fb41893aa802ad3c1e05
Tanush Vishwakarma <vishwakarmatanush052@gmail.com>
2025-07-21 10:52:44 +0530

    Create os-info.js
    by using op(operating system)
    fetch system info

---
+++ Day3/os-info.js
@@ -0,0 +1,7 @@
+const os = require('os');
+
+console.log('Operating System:', os.platform());
+console.log('CPU Architecture:', os.arch());
+console.log('CPU Info:', os.cpus());
+console.log('Free Memory:', os.freemem());
+console.log('Home Directory:', os.homedir());
