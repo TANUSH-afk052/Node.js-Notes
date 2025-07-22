39faf45c832bcc7da04203201061047afdd6037f
Tanush Vishwakarma <vishwakarmatanush052@gmail.com>
2025-07-22 11:01:36 +0530

    Update app.js

--- Day2/app.js
+++ Day2/app.js
@@ -1,2 +1,5 @@
 const add = require('./math');
-console.log(add(5, 10)); // Output: 15
+
+console.log(math.add(5, 10));       // Output: 15
+console.log(math.multiply(5, 10));  // Output: 50
+console.log(math.divide(10, 2));    // Output: 5
